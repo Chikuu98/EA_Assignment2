bdbfd8d0909d23336884e27cf366663bc8239aaf
Chikuu98 <chiranjonline@gmail.com>
2021-12-05 12:29:15 +0530

    database connected Signed-off-by: Chikuu98 <chiranjonline@gmail.com>

---
+++ src/assignment2/DriverManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import com.mysql.jdbc.Connection;
+
+/**
+ *
+ * @author Intel
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhostskillsreg, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
